aabef7db305940f2a7c38f0a5251596e7b5ce692
Your Name <you@example.com>
2023-04-27 19:26:06 +0700

    add

--- bai1.cpp
+++ bai1.cpp
@@ -1,5 +1,26 @@
-#include<stdio.h>
-int main(){
-    int n;
-    printf("co su thay doi lon o day nha");
+#include <bits/stdc++.h>
+using namespace std;
+  
+// Recursive function to 
+// reverse digits of num
+int reverseDigits(int num)
+{
+    static int rev_num = 0;
+    static int base_pos = 1;
+    if (num > 0) 
+    {
+        reverseDigits(num / 10);
+        rev_num += (num % 10) * base_pos;
+        base_pos *= 10;
+    }
+    return rev_num;
+}
+  
+// Driver Code
+int main()
+{
+    int num = 4562;
+    cout << "Reverse of no. is " << 
+             reverseDigits(num);
+    return 0;
 }
